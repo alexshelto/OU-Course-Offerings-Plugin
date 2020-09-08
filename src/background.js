3ba09284f18e041ae60314945fb6d69a0b00c501
alexshelto <alexandershelton@icloud.com>
2020-09-08 14:39:51 -0400

    scrapes all professors names from course offerings

--- src/background.js
+++ src/background.js
@@ -25,7 +25,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 function afterInject(){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {action: "getNumOfTables"}, function(result) {
-      // console.log(result);
+      console.log(result);
     });
   });
 }
--- src/content.js
+++ src/content.js
@@ -1,5 +1,10 @@
 // content.js
 
+
+instructorStore = {
+
+}
+
 // Ensure it's injected only once
 var injected;
 if(!injected) {
@@ -7,15 +12,37 @@ if(!injected) {
   chrome.runtime.onMessage.addListener(handleMessage);
 }
 
+//logic here
+function scrapeProfessors() {
+  $('.sectionDetail').each(function () {
+    if($(this).find("td").length > 0) {
+      let instructorName = $(this).find("td")[6].innerText.trim(); //grabbing professors name
+      //adding the instructor to the dictionary if not currently in
+      if([instructorStore[instructorName] === undefined]) {
+        // instructorStore[instructorName] = getRateMyProfessorScore(instructorName);
+        instructorStore[instructorName] = 'N/A';
+      }
+    }
+  })
+}
 
 
+// function getRateMyProfessorScore(name) {
 
+// }
+
+//First 3 listings selectors
+// #courseATHN1000 > tbody > tr:nth-child(2) > td.columnTitle
+// #courseATHN2000 > tbody > tr:nth-child(2) > td.columnTitle
+// #courseATHN3000 > tbody > tr:nth-child(2) > td.columnValue
 
 function handleMessage(message, sender, sendResponse) {
   console.log(`Recieved the message: ${message.action}`);
   switch(message.action) {
     case "getNumOfTables":
-      sendResponse($(document).find("table").length);
+      scrapeProfessors(); //scrapes all instructor names from course offerings into key value pair
+      sendResponse($(document).find("td:contains('Instructors:)").length);
+
     default:
       console.error("Unknown request from extension");
   }
