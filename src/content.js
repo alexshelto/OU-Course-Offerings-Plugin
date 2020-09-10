625c4f332af9ebb94522e435d502bdf154c87f99
alexshelto <alexandershelton@icloud.com>
2020-09-09 23:25:55 -0400

    some es6 functions

--- src/content.js
+++ src/content.js
@@ -1,12 +1,12 @@
 // content.js
 
 
-// https://www.ratemyprofessors.com/filter/professor/?&page=1&queryoption=TEACHER&query=Juanita+Hall&sid=727
 // https://www.ratemyprofessors.com/filter/professor/?&page=1&queryoption=TEACHER&query=David+Juedes
 
 var rate_my_prof_url = 'https://www.ratemyprofessors.com/filter/professor/?&page=1&queryoption=TEACHER&query=';
 var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
+var instructorStore = {};
 
 
 // Ensure it's injected only once
@@ -19,7 +19,7 @@ if(!injected) {
 
 
 
-function getInstrcutorScore(name, callback) {
+const getInstrcutorScore = (name, callback) => {
   var score;
   fetch(`${proxyUrl}${rate_my_prof_url}${name.split(' ').join('+')}&sid=727`) //727 is school id code for ohio university
     .then(
@@ -49,24 +49,35 @@ function getInstrcutorScore(name, callback) {
 }
 
 
+// function injectScore(elements, value) {
+//   var td = document.createElement('h1');
+//   elements.append(td);
+//   td.innerText = value;
+// }
+
 
 //logic here
-function scrapeProfessors() {
+const scrapeProfessors = () => {
   let uniqueInstructors = 0;
-  let instructorStore = {};
   $('.sectionDetail').each(function () {
     if($(this).find("td").length > 0) {
       let instructorName = $(this).find("td")[6].innerText.trim(); //grabbing professors name. weird formatting like:          prof  name         
       
       //adding the instructor to the dictionary if not currently in
       if(!(instructorName in instructorStore)) {
-        let score = getInstrcutorScore(instructorName, (score) => {
+        let score = getInstrcutorScore(instructorName, (score) => { ///////FIX THIS LINE
           instructorStore[instructorName] = score;
         });
         uniqueInstructors += 1; //iterating unique instructors 
-      }
-    }
-  });
+      }//endif
+      //inject score.
+      var td = document.createElement('td');
+      $(this).find('td').append(td);
+      td.innerText = `${instructorStore}: RMP Score: ${instructorStore[instructorName]}`;
+      // injectScore($(this).find("td"), instructorStore[instructorName]);
+    }//endif
+
+  })
   //store of names
   console.log(instructorStore);
   console.log(`Unique Instructors: ${uniqueInstructors}`);
@@ -76,7 +87,7 @@ function scrapeProfessors() {
 
 
 
-function handleMessage(message, sender, sendResponse) {
+function handleMessage(message, sender, sendResponse){
   console.log(`Recieved the message: ${message.action}`);
   switch(message.action) {
     case "getNumOfTables":
