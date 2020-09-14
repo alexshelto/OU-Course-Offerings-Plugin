ae85e8b9e21825ee2f63107f32f7cfbe250e4309
alexshelto <alexandershelton@icloud.com>
2020-09-13 22:51:34 -0400

    plugin access kv server

--- src/content.js
+++ src/content.js
@@ -1,12 +1,4 @@
-// content.js
 
-
-// https://www.ratemyprofessors.com/filter/professor/?&page=1&queryoption=TEACHER&query=David+Juedes
-
-var rate_my_prof_url = 'https://www.ratemyprofessors.com/filter/professor/?&page=1&queryoption=TEACHER&query=';
-var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-
-var instructorStore = {};
 
 
 // Ensure it's injected only once
@@ -16,37 +8,25 @@ if(!injected) {
   chrome.runtime.onMessage.addListener(handleMessage);
 }
 
+const URL = "http://127.0.0.1:5000/"
 
 
 
-const getInstrcutorScore = (name, callback) => {
-  var score;
-  fetch(`${proxyUrl}${rate_my_prof_url}${name.split(' ').join('+')}&sid=727`) //727 is school id code for ohio university
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log(`Error: ${response.status}`);
-          return;
-        }
-        //Examine response
-        response.json().then(function(data) {
-          //data.professors returns an array with details on each prof. professor[1] is our desired prof
 
-          if(data.professors.length == 0) callback(-1); //clean up
-          // if(data.professors.length > 1) score = -1
-          else {
-            console.log(data.professors[0]['overall_rating']);
-            callback(data.professors[0]['overall_rating']);
-          }
-        });
-      }
-    ).catch(function(err) {
-      console.log(`Error: ${err}`);
-      callback(-1); //clean  
-    });
 
-    callback(score);
+async function getInstrcutorScore(name) {
+  let score = 'N/A'
+  console.log(`Inside getInstructorScore: ${name}`);
+  let initialResponse = await fetch(`${URL}api/${name}`);
+  let data = await initialResponse.json();
+  if(data.data.length > 0){
+    score = data.data[0]['score'];
+  }
+  console.log(`return score: ${score}`);
+  return score;
 }
+
+
 
 
 // function injectScore(elements, value) {
@@ -56,31 +36,32 @@ const getInstrcutorScore = (name, callback) => {
 // }
 
 
+
+
+
 //logic here
-const scrapeProfessors = () => {
+function scrapeProfessors() {
+  let store = {};
+  let n = 0;
+
   let uniqueInstructors = 0;
   $('.sectionDetail').each(function () {
     if($(this).find("td").length > 0) {
       let instructorName = $(this).find("td")[6].innerText.trim(); //grabbing professors name. weird formatting like:          prof  name         
-      
-      //adding the instructor to the dictionary if not currently in
-      if(!(instructorName in instructorStore)) {
-        let score = getInstrcutorScore(instructorName, (score) => { ///////FIX THIS LINE
-          instructorStore[instructorName] = score;
-        });
-        uniqueInstructors += 1; //iterating unique instructors 
-      }//endif
-      //inject score.
-      var td = document.createElement('td');
-      $(this).find('td').append(td);
-      td.innerText = `${instructorStore}: RMP Score: ${instructorStore[instructorName]}`;
-      // injectScore($(this).find("td"), instructorStore[instructorName]);
-    }//endif
+      instructorName  = instructorName.split(' ').join(''); // sting before: John Smith, after: JohnSmith
+      console.log(`Prof #${n}, name:${instructorName}`);
 
-  })
-  //store of names
-  console.log(instructorStore);
+      getInstrcutorScore(instructorName).
+      then((score) => {
+        store[instructorName] = score;
+        console.log(score);
+      }).catch(err => console.log(err));
+      n += 1;
+
+    }//endif
+});
   console.log(`Unique Instructors: ${uniqueInstructors}`);
+  console.log(store);
 }
 
 
@@ -89,12 +70,12 @@ const scrapeProfessors = () => {
 
 function handleMessage(message, sender, sendResponse){
   console.log(`Recieved the message: ${message.action}`);
-  switch(message.action) {
-    case "getNumOfTables":
-      scrapeProfessors(); //scrapes all instructor names from course offerings into key value pair
-      sendResponse($(document).find("td:contains('Instructors:)").length);
 
-    default:
-      console.error("Unknown request from extension");
+  if(message.action == "inject-professors"){
+    scrapeProfessors();
+    sendResponse($(document).find("td:contains('Instructors:)").length);
+  }
+  else {
+    console.error("Unknown request from extension");
   }
 }
